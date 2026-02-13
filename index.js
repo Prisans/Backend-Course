6f70988f3698f3dd0e85a92d5594d750430b63fe
Prisans <124236508+Prisans@users.noreply.github.com>
2026-02-13 20:34:50 +0530

    patch method, middlewares added

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const users = require("./MOCK_DATA.json")
 
 const app = express()
 
+// middlewares ------------------------------->
+
 app.use(express.urlencoded({extended : true}))
 
-// routes
+app.use((req,res,next)=>{
+    console.log("hello from middleware 1")
+    next()
+})
+
+app.use((req,res,next)=>{
+    fs.appendFile("log.txt" , ` \n ${Date.now()} : ${req.ip} : ${req.path} : ${req.method}` , (err,data)=>{
+        next()
+    })
+})
+
+// routes and http methods -------------------------------------------->
 app.get("/",(req,res)=>{
     res.send("hello prisans")
 })
 
 // console.log(users)
 
-// rest api
+// rest api -------------------------->
 
 app.get("/users" , (req,res)=>{
     const html = `
@@ -28,7 +41,7 @@ app.get("/api/users" , (req,res)=>{
     return res.json(users)
 })
 
-// dynamic routes
+// dynamic routes ------------------>
 
 app.get("/api/users/:id" , (req,res)=>{
     const userId = Number(req.params.id) 
@@ -42,6 +55,8 @@ app.get("/api/users/:id" , (req,res)=>{
     return res.json(newUser)
 })
 
+// post method--------->
+
 app.post("/api/users" , (req,res)=>{
     const body = req.body
     users.push({...body, id : users.length +1})
@@ -52,6 +67,42 @@ app.post("/api/users" , (req,res)=>{
     return res.json({status : "pending"})
 })
 
+// delete method ---------------->
+
+app.delete("/users/:id" , (req,res)=>{
+    const deletedId = Number(req.params.id)
+    const deletedData = users.filter((item)=>{
+        return item.id !=deletedId
+    })
+    
+    console.log(deletedData)
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(deletedData) , (err,data)=>{
+        return res.json({status : "deleted"})
+    })
+})
+
+// patch ( update method) ----------------->
+
+app.patch("/users/:id" , (req,res)=>{
+    const updatedId = Number(req.params.id)
+    const updatedData = req.body
+
+    let updatedUser = users.map((item)=>{
+        return (
+            item.id === updatedId ? {...item , ...updatedData} : item
+        )
+    })
+
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(updatedUser) , (err,data)=>{
+        return res.json({status : "updated"})
+    })
+
+    // console.log(updatedUser)
+
+})
+
+
+// running server ---------------->
 app.listen(8000,()=>{
     console.log("server startedd")
 })
